8cd5f0b6f36248f107170b8ae8b0e57bfa732035
songxinlu <337946745@qq.com>
2021-10-25 16:50:10 +0800

    springTask使用案例提交

--- springtask/src/main/java/com/example/demo/SpringtaskApplication.java
+++ springtask/src/main/java/com/example/demo/SpringtaskApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootstudy;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootStudyApplication {
+public class SpringtaskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootStudyApplication.class, args);
+        SpringApplication.run(SpringtaskApplication.class, args);
     }
 
 }
